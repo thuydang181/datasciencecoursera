382d2e23c9ed859817e564552eb5e16d0fbd3624
Thuy Dang <59451211+thuydang181@users.noreply.github.com>
2020-05-10 20:15:41 -0400

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -8,18 +8,18 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 ## This function creats a special "matrix" object that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) { ##define the argument with default mode of "matrix"
-	inv <- NULL 				  ##initialize inv as NULL,it will hold value of matrix inverse
-	set <- function(y) {			  ##define the set function to assign new value
-		x <<- y 				  ##of matrix in parent environment		
-		inv <<- NULL 			  ##if there is a new matrix, reset inv to NULL
+makeCacheMatrix <- function(x = matrix()) { ## define the argument with default mode of "matrix"
+	inv <- NULL 				  ## initialize inv as NULL,it will hold value of matrix inverse
+	set <- function(y) {			  ## define the set function to assign new value
+		x <<- y 		          ## of matrix in parent environment		
+		inv <<- NULL 			  ## if there is a new matrix, reset inv to NULL
 	}
-	get <- function() x			  ##define the get function that returns value of the matrix argument 
+	get <- function() x			  ## define the get function that returns value of the matrix argument 
 
-	setinverse <- function(inverse) inv <<- inverse ##assigns value of inv in parent environment
-	getinverse <- function() inv 				##gets the value of inv where called
-	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ##this is for referring
-														##referring to the functions with $ operator
+	setinverse <- function(inverse) inv <<- inverse   ## assigns value of inv in parent environment
+	getinverse <- function() inv 		          ## gets the value of inv where called
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  ## this is for referring
+										      ## to the functions with $ operator
 }		 
 
 ## Write a short comment describing this function
@@ -31,19 +31,12 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 	inv <- x$getinverse()
 	if (!is.null(inv)) {
-		message{"getting cached data")
+		message("getting cached data")
 		return(inv)
 	}
 	data <- x$get()
-        m <- mean(data, ...)
+	inv <- solve(data, ...)
         x$setinverse(inv)
         inv
-{data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m}data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
 }	
-}
+
